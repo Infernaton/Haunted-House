d2d3936fd1f766cf52d5d4a76e0d2cd4e115a5dd
Infernaton <lubin.pauly@gmail.com>
2024-01-09 17:46:25 +0100

    Game Loop

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public enum GameState
+{
+    Menu, // In the gameMenu Before the game itself
+    WaitingMonster,
+    InComming,
+    EndGame // End game state
+}
 
 public class GameManager : MonoBehaviour
 {
+    private GameState _state;
     [SerializeField] PlayerController m_Player;
     [SerializeField] CameraManager m_Camera;
     public PlayerController Player() => m_Player;
     public CameraManager Camera() => m_Camera;
+
 
     public static GameManager Instance; // A static reference to the GameManager instance
     void Awake()
@@ -20,9 +29,65 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject); // Destroy the GameObject, this component is attached to
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        ResetMonster();
+    }
+
+    private void Update()
+    {
+        switch (_state)
+        {
+            case GameState.WaitingMonster:
+                break;
+            case GameState.InComming:
+                if (!m_Player.isHidden) LoseGame();
+                break;
+            case GameState.Menu:
+            case GameState.EndGame:
+            default: break;
+        }
+    }
+
+    private void FirstWarning()
+    {
+        Debug.Log("First Warning");
+        Invoke(nameof(SecondWarning), 10f);
+    }
+
+    private void SecondWarning()
+    {
+        Debug.Log("Second Warning");
+        Invoke(nameof(LastWarning), 5f);
+    }
+
+    private void LastWarning()
+    {
+        Debug.Log("Last Warning");
+        Invoke(nameof(InComming), 5f);
+    }
+
+    private void InComming()
+    {
+        Debug.Log("In Comming");
+        _state = GameState.InComming;
+        Invoke(nameof(ResetMonster), 5f);
+    }
+
+    private void ResetMonster()
+    {
+        Debug.Log("Reset");
+        _state = GameState.WaitingMonster;
+        Invoke(nameof(FirstWarning), 10f);
+    }
+
+    private void WinGame()
+    {
+        // Finishing Game
+    }
+
+    private void LoseGame()
+    {
+        //Set Animation Lose Game
     }
 }
