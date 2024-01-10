ffa438ff1d27d3bd0360ac4b04648f8d1b916a0d
Infernaton <lubin.pauly@gmail.com>
2024-01-10 15:00:06 +0100

    Title Screen

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,14 +7,21 @@ using Utils;
 public class Menu : MonoBehaviour
 {
     [SerializeField] RawImage m_BlackScreen;
+    [SerializeField] RawImage m_SignBG = null;
 
     public void Start()
     {
         StartCoroutine(Anim.FadeOut(1f, m_BlackScreen));
     }
-    public void Play()
+    public void Play(Texture texture = null)
     {
+        if (texture != null) m_SignBG.texture = texture;
         StartCoroutine(FadeTransition("FinalScene"));
+    }
+    public void New(Texture texture = null)
+    {
+        Debug.Log("test");
+        if (texture != null) m_SignBG.texture = texture;
     }
 
     public void MainMenu()
@@ -29,12 +36,15 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene(scene);
     }
 
-    public void Quit()
+    
+
+    public void Quit(Texture texture = null)
     {
         //Quit app
     #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-    #endif
+#endif
+        if (texture != null) m_SignBG.texture = texture;
         Application.Quit();
     }
 }
